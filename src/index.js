e4324dc2f086494b3686f856cb0167150d39f4a0
Greg <gregoryminezzi@gmail.com>
2024-08-21 10:35:27 +0200

    close menu on link selection

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
 const hamburger = document.getElementById("hamburger");
 const menu = document.getElementById("menu");
-const header = document.getElementById("header")
+const header = document.getElementById("header");
+const links = document.querySelectorAll("#menu a");
 
-hamburger.addEventListener("click", () => {
-  // main page slides to left to reveal navbar
-  header.classList.toggle("-translate-x-[250px]")
-  // view/hide menu
-  menu.classList.toggle("hidden");
-  // hamburger turns into x
-  hamburger.firstElementChild.classList.toggle("rotate-[35deg]");
-  hamburger.firstElementChild.classList.toggle("translate-y-[6px]");
-  hamburger.children[1].classList.toggle("scale-0");
-  hamburger.lastElementChild.classList.toggle("-rotate-[35deg]");
-  hamburger.lastElementChild.classList.toggle("-translate-y-[6px]");
+const toggleMenu = () => {
+    // main page slides to left to reveal navbar
+    header.classList.toggle("-translate-x-[250px]");
+    // view/hide menu
+    menu.classList.toggle("hidden");
+    // hamburger turns into x
+    hamburger.firstElementChild.classList.toggle("rotate-[35deg]");
+    hamburger.firstElementChild.classList.toggle("translate-y-[6px]");
+    hamburger.children[1].classList.toggle("scale-0");
+    hamburger.lastElementChild.classList.toggle("-rotate-[35deg]");
+    hamburger.lastElementChild.classList.toggle("-translate-y-[6px]");
+};
+// toggle menu on hamburger click
+hamburger.addEventListener("click", toggleMenu);
+
+// close the menu on link selection
+links.forEach(link => {
+  link.addEventListener("click", toggleMenu);
 });
-
-
